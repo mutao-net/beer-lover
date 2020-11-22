711101c78bd0a6c8e2c543726e8e4286a1cf08d4
mutao-net <sasaki.h1026@gmail.com>
2020-11-22 13:40:19 +0900

    とりあえず表示させるまで

--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
 from flask import Flask,render_template
 import requests
+import json
 
 app = Flask(__name__)
 
 # https://punkapi.com/documentation/v2
 @app.route("/")
 def index():
-    url="https://api.punkapi.com/v2/beers/random"
-    response = requests.get(url)
-    return render_template("index.html", response=response.text)
+    url="https://api.punkapi.com/v2/beers"
+    headers = {"content-type": "application/json"}
+    response = requests.get(url, headers).json()
+    return render_template("index.html", response=response)
 
 
 #おまじない
